52c75b050eb1e4ffc8b572341d0975c8aff2644e
jesusemendoza <je.mendoza@live.com>
2020-06-07 19:49:25 -0700

    video output/view working on chrome

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
 //jshint eversion: 6
 
 const express = require("express");
-const http = require("http");
 const path = require("path");
 const app = express();
-const staticRouter = require("./routes/static");
+const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
+const staticRouter = require("./routes/static");
 
 const PORT = process.env.PORT || 3000;
 
-
-
-
-const sever = http.Server(app);
-
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", staticRouter);
 
-io.on('connection', function(socket){
-  socket.on('stream',function(image){
-    socket.broadcast.emit('stream',image);
-  })
-})
+io.on("connection", function (socket) {
+  socket.on("stream", function (image) {
+    socket.broadcast.emit("stream", image);
+  });
+});
 
-
-
-
-
-app.listen(PORT, function(){
-  console.log("server is running in " ,PORT);
-})
+http.listen(PORT, function () {
+  console.log("server is running in ", PORT);
+});
